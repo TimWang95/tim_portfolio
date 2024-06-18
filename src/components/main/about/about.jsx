9d509e7886ffce587d6d04c16df5678369fe7959
tgm761 <tgm761@gmail.com>
2024-06-18 18:12:34 +0800

    add section of about

---
+++ src/components/main/about/about.jsx
@@ -0,0 +1,12 @@
+import 'src/components/main/about/about.scss';
+
+export default function About(){
+  return (
+    <section className="about" id="about">
+      <h2>About Me</h2>
+      <p>「Always be better than yesterday」</p>
+      <p>您好，我是天佑！喜歡嘗試新事物，注重視覺美感與設計能力，致力成為具有設計與工程能力並存的工程師。
+期許自己持續精進技術實力，不停止學習，協助公司解決問題，一同成長。</p>
+    </section>
+  )
+}
